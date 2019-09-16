59220f453549666ef5488fb4678482c3ade16ea8
nperperidis <perpegr@hotmail.com>
2019-09-16 22:58:49 +0300

    init

---
+++ IpInformation/IpInfoProvider/IPServiceNotAvailableException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IpInfoProvider
+{
+    public class IPServiceNotAvailableException : Exception
+    {
+        public IPServiceNotAvailableException() : base("Service is not Available")
+        {
+
+        }
+    }
+}
